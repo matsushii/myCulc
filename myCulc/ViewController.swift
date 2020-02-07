892eab2811f81bcf6e3c767ec6823dccb423e73c
matsushii <mtsmath24@yahoo.co.jp>
2020-02-08 00:30:10 +0900

    Initial Commit

---
+++ myCulc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myCulc
+//
+//  Created by M.Atsuhi on 2020/02/08.
+//  Copyright © 2020 M.Atsuhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
