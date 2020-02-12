0ccbb39a4a455c6865aa72b32d7eafd9c2a8a706
matsushii <mtsmath24@yahoo.co.jp>
2020-02-12 18:47:19 +0900

    UIviewDidLoadを呼び出す記述および1ボタンをタップした際の処理を記述
    UIviewDidLoadを呼び出す記述および1ボタンをタップした際の処理を記述した。

--- myCulc/ViewController.swift
+++ myCulc/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    //IB:インターフェースビルダーの略
+    //weak:オーナーシップを持たない（オブジェクトを参照しない）
+    @IBOutlet weak var priceFfield: UITextField!
     override func viewDidLoad() {
+        //  親クラスのUIViewControllerのviewDidLaodを呼び出す
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //  既存の処理を行った後に追加で行う処理を記述
     }
-
-
+    //  @IBAction：StoryBoardと関連していることを示す
+    @IBAction func top1Button(_ sender: Any) {
+        //  priceFieldのtextに1を追加した文字列をvalueとして定義
+        let value = priceFfield.text! + "1"
+        //  valueを整数型にアンラップしてpriceとして定義
+        if  let price =  Int(value) {
+            //  priceFfieldにpriceに代入
+            priceFfield.text! = "\(price)"
+        }
+    }
 }
-
